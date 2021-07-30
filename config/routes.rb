5064074a6761e70036e4ffb93793c6b8d5c9ec36
ryshino <74131105+ryshino@users.noreply.github.com>
2021-07-31 07:32:28 +0900

    station7クリア！

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   resources :movies, path: '/admin/movies'
+  resources :sheets
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+5.times do |n|
+  Sheet.create(:column => n+1, :row => 'a')
+end
+
+5.times do |n|
+  Sheet.create(:column => n+1, :row => 'b')
+end
+
+5.times do |n|
+  Sheet.create(:column => n+1, :row => 'c')
+end
