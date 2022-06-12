ad2d89819e856c59597f54013e6991785501ace7
ryshino <ranature1153@gmail.com>
2022-06-12 11:08:59 +0900

    seeds.rbを本番環境に合わせた形に変更

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,75 +6,75 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-2.times do |n|
-  Theater.create(:name => "映画館#{n+1}")
+
+Theater.create(:id => 1, :name => "映画館1")
+Theater.create(:id => 2, :name => "映画館2")
+
+3.times do |n|
+  Screen.create(:id => n+1, :screen => n+1, :theater_id => 1)
 end
 
 3.times do |n|
-  Screen.create(:screen => n+1, :theater_id => 1)
+  Screen.create(:id => n+4, :screen => n+1, :theater_id => 2)
 end
 
-3.times do |n|
-  Screen.create(:screen => n+1, :theater_id => 2)
-end
-
-(0..14).each do |n|
+(0..14).each_with_index do | n, i|
  if n <= 4
-  Sheet.create(:column => n+1, :row => 'a', :screen_id => 1, :theater_id => 1)
+  Sheet.create(:id => i+1, :column => n+1, :row => 'a', :screen_id => 1, :theater_id => 1)
  elsif n <= 9
-  Sheet.create(:column => n+1, :row => 'b', :screen_id => 1, :theater_id => 1)
+  Sheet.create(:id => i+1, :column => n+1, :row => 'b', :screen_id => 1, :theater_id => 1)
  elsif n <= 14
-  Sheet.create(:column => n+1, :row => 'c', :screen_id => 1, :theater_id => 1)
+  Sheet.create(:id => i+1, :column => n+1, :row => 'c', :screen_id => 1, :theater_id => 1)
  end
 end
 
-(0..14).each do |n|
+(0..14).each_with_index do | n, i|
   if n <= 4
-   Sheet.create(:column => n+1, :row => 'a', :screen_id => 2, :theater_id => 1)
+   Sheet.create(:id => i+16, :column => n+1, :row => 'a', :screen_id => 2, :theater_id => 1)
   elsif n <= 9
-   Sheet.create(:column => n+1, :row => 'b', :screen_id => 2, :theater_id => 1)
+   Sheet.create(:id => i+16, :column => n+1, :row => 'b', :screen_id => 2, :theater_id => 1)
   elsif n <= 14
-   Sheet.create(:column => n+1, :row => 'c', :screen_id => 2, :theater_id => 1)
+   Sheet.create(:id => i+16, :column => n+1, :row => 'c', :screen_id => 2, :theater_id => 1)
   end
  end
 
- (0..14).each do |n|
+ (0..14).each_with_index do | n, i|
   if n <= 4
-   Sheet.create(:column => n+1, :row => 'a', :screen_id => 3, :theater_id => 1)
+   Sheet.create(:id => i+31, :column => n+1, :row => 'a', :screen_id => 3, :theater_id => 1)
   elsif n <= 9
-   Sheet.create(:column => n+1, :row => 'b', :screen_id => 3, :theater_id => 1)
+   Sheet.create(:id => i+31, :column => n+1, :row => 'b', :screen_id => 3, :theater_id => 1)
   elsif n <= 14
-   Sheet.create(:column => n+1, :row => 'c', :screen_id => 3, :theater_id => 1)
+   Sheet.create(:id => i+31, :column => n+1, :row => 'c', :screen_id => 3, :theater_id => 1)
   end
  end
 
- (0..14).each do |n|
+ (0..14).each_with_index do | n, i|
   if n <= 4
-   Sheet.create(:column => n+1, :row => 'a', :screen_id => 1, :theater_id => 1)
+   Sheet.create(:id => i+46, :column => n+1, :row => 'a', :screen_id => 1, :theater_id => 2)
   elsif n <= 9
-   Sheet.create(:column => n+1, :row => 'b', :screen_id => 1, :theater_id => 1)
+   Sheet.create(:id => i+46, :column => n+1, :row => 'b', :screen_id => 1, :theater_id => 2)
   elsif n <= 14
-   Sheet.create(:column => n+1, :row => 'c', :screen_id => 1, :theater_id => 1)
+   Sheet.create(:id => i+46, :column => n+1, :row => 'c', :screen_id => 1, :theater_id => 2)
   end
  end
- 
- (0..14).each do |n|
+
+ (0..14).each_with_index do | n, i|
    if n <= 4
-    Sheet.create(:column => n+1, :row => 'a', :screen_id => 2, :theater_id => 2)
+    Sheet.create(:id => i+61, :column => n+1, :row => 'a', :screen_id => 2, :theater_id => 2)
    elsif n <= 9
-    Sheet.create(:column => n+1, :row => 'b', :screen_id => 2, :theater_id => 2)
+    Sheet.create(:id => i+61, :column => n+1, :row => 'b', :screen_id => 2, :theater_id => 2)
    elsif n <= 14
-    Sheet.create(:column => n+1, :row => 'c', :screen_id => 2, :theater_id => 2)
+    Sheet.create(:id => i+61, :column => n+1, :row => 'c', :screen_id => 2, :theater_id => 2)
    end
   end
  
-  (0..14).each do |n|
+(0..14).each_with_index do | n, i|
    if n <= 4
-    Sheet.create(:column => n+1, :row => 'a', :screen_id => 3, :theater_id => 2)
+    Sheet.create(:id => i+76, :column => n+1, :row => 'a', :screen_id => 3, :theater_id => 2)
    elsif n <= 9
-    Sheet.create(:column => n+1, :row => 'b', :screen_id => 3, :theater_id => 2)
+    Sheet.create(:id => i+76, :column => n+1, :row => 'b', :screen_id => 3, :theater_id => 2)
    elsif n <= 14
-    Sheet.create(:column => n+1, :row => 'c', :screen_id => 3, :theater_id => 2)
+    Sheet.create(:id => i+76, :column => n+1, :row => 'c', :screen_id => 3, :theater_id => 2)
    end
   end
 
