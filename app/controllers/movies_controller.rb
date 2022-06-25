9013f0dca66d75c227d34326285d22bb8831e50e
ryshino <ranature1153@gmail.com>
2022-06-25 16:15:57 +0900

    日付を選択してない時に全てのスケジュールを選べてしまう仕様を修正

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -10,15 +10,20 @@ class MoviesController < ApplicationController
 
   def schedule_select
     @date = params[:date]
+
+   if @date.blank?
+    @schedules = ["選択可能なスケジュールがありません"]
+   else
     @movie_id = params[:movie_id]
     @movie = Movie.find(@movie_id)
     @schedules = Schedule.where(movie_id: @movie.id)
     @schedules = @schedules.where('start_time LIKE ?', "#{@date}%").pluck(:start_time, :id)
-    
-   if @schedules.blank?
-    @schedules = ["選択可能なスケジュールがありません"]
-   else 
-     @schedules
+
+    if @schedules.blank?
+      @schedules = ["選択可能なスケジュールがありません"]
+     else 
+       @schedules
+     end
    end
   end
   
