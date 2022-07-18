7b3192bc49903c7b78f3575e5e0b9ace91873229
ryshino <ranature1153@gmail.com>
2022-07-18 16:01:48 +0900

    ユーザーの予約一覧で重複して表示される問題を解決

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   before_action :set_user, only: [:show]
 
   def show
-    @movies = Movie.joins(schedules: :reservations).where(reservations: { date: Time.zone.now.beginning_of_day...Time.zone.now.since(7.days).end_of_day, user_id: @user.id })
+    @movies = Movie.joins(schedules: :reservations).group(:id).where(reservations: { date: Time.zone.now.beginning_of_day...Time.zone.now.since(7.days).end_of_day, user_id: @user.id })
   end
   
   private
